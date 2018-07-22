f79f267835478d2dabbdf2d9e77a0c9637d60488
RELNO <noyman@ariels-mbp.mynet>
2018-07-22 15:54:03 +0300

    WIP update bufferGeo locs

--- scripts/peds.js
+++ scripts/peds.js
@@ -1,5 +1,7 @@
 // https://github.com/mrdoob/three.js/blob/master/examples/webgl_buffergeometry_drawcalls.html
 import * as THREE from "THREE";
+var TWEEN = require("@tweenjs/tween.js");
+import { tweenVector3 } from "./tweenModules";
 
 export function makePeds(color) {
   var peds;
@@ -7,7 +9,7 @@ export function makePeds(color) {
   var geometry = new THREE.BufferGeometry();
   var positions = [];
   var colors = [];
-  //size of bouding box in THREE units
+  //size of bounding box in THREE units
   var n = 0.5,
     n2 = n / 2; // particles spread in the cube
   for (var i = 0; i < particles; i++) {
@@ -19,20 +21,37 @@ export function makePeds(color) {
     // colors -WIP GET THIS IN RGB
     colors.push(color[0], color[1], color[2]);
   }
-
   geometry.addAttribute(
     "position",
     new THREE.Float32BufferAttribute(positions, 3)
   );
   geometry.addAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
-
   geometry.computeBoundingSphere();
-
   var material = new THREE.PointsMaterial({
     size: 0.01,
     vertexColors: THREE.VertexColors
   });
   peds = new THREE.Points(geometry, material);
 
+  // let p = peds.geometry.attributes.position.array;
+
+  // for (let i = 0; i < p.length - 3; i = i + 3) {
+  //   pedPos(p[i], p[i + 1], [p + 2]);
+  // }
+
   return peds;
+}
+
+//position Usage
+function pedPos(e) {
+  var target = new THREE.Vector3(e[0] + 10, e[1], e[2] + 10); // create on init
+  tweenVector3(e.position, target, {
+    duration: rndInt(0, 2000),
+    easing: TWEEN.Easing.Quadratic.InOut
+  });
+}
+
+////////////////////////////////////////
+function rndInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
--- scripts/searchNearest.js
+++ scripts/searchNearest.js
@@ -27,8 +27,9 @@ export function searchNearest(thisType, searchType, grid, x, y) {
       let finalCol = remapCol(countRes);
       drawCell(grid.children[i], finalCol, 2000);
       // grid.children[i].scale.set(1, countRes + 1, 1);
-      //pedestrians per object
-      let peds = PEDS.makePeds(hslToRgb(countRes, 1, 0.5));
+
+      //add pedestrians per grid object
+      let peds = PEDS.makePeds(hslToRgb(countRes * 100, 1, 0.5));
       grid.children[i].add(peds);
     }
   }
@@ -72,38 +73,56 @@ function remapCol(n) {
 }
 
 ////////////////////////////////////////
-/**
- * Converts an HSL color value to RGB. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes h, s, and l are contained in the set [0, 1] and
- * returns r, g, and b in the set [0, 255].
- *
- * @param   Number  h       The hue
- * @param   Number  s       The saturation
- * @param   Number  l       The lightness
- * @return  Array           The RGB representation
- */
-function hslToRgb(h, s, l) {
-  var r, g, b;
+// based on algorithm from http://en.wikipedia.org/wiki/HSL_and_HSV#Converting_to_RGB
 
-  if (s == 0) {
-    r = g = b = l; // achromatic
-  } else {
-    function hue2rgb(p, q, t) {
-      if (t < 0) t += 1;
-      if (t > 1) t -= 1;
-      if (t < 1 / 6) return p + (q - p) * 6 * t;
-      if (t < 1 / 2) return q;
-      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-      return p;
-    }
-
-    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    var p = 2 * l - q;
-    r = hue2rgb(p, q, h + 1 / 3);
-    g = hue2rgb(p, q, h);
-    b = hue2rgb(p, q, h - 1 / 3);
+function hslToRgb(hue, saturation, lightness) {
+  if (hue == undefined) {
+    return [0, 0, 0];
   }
 
-  return [r * 255, g * 255, b * 255];
+  var chroma = (1 - Math.abs(2 * lightness - 1)) * saturation;
+  var huePrime = hue / 60;
+  var secondComponent = chroma * (1 - Math.abs((huePrime % 2) - 1));
+
+  huePrime = Math.floor(huePrime);
+  var red;
+  var green;
+  var blue;
+
+  if (huePrime === 0) {
+    red = chroma;
+    green = secondComponent;
+    blue = 0;
+  } else if (huePrime === 1) {
+    red = secondComponent;
+    green = chroma;
+    blue = 0;
+  } else if (huePrime === 2) {
+    red = 0;
+    green = chroma;
+    blue = secondComponent;
+  } else if (huePrime === 3) {
+    red = 0;
+    green = secondComponent;
+    blue = chroma;
+  } else if (huePrime === 4) {
+    red = secondComponent;
+    green = 0;
+    blue = chroma;
+  } else if (huePrime === 5) {
+    red = chroma;
+    green = 0;
+    blue = secondComponent;
+  }
+
+  var lightnessAdjustment = lightness - chroma / 2;
+  red += lightnessAdjustment;
+  green += lightnessAdjustment;
+  blue += lightnessAdjustment;
+
+  return [
+    Math.round(red * 255),
+    Math.round(green * 255),
+    Math.round(blue * 255)
+  ];
 }
