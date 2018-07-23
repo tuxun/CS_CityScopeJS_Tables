fdac686a2bbafe2c2fddf00c30ae04de5cb4447a
RELNO <noyman@ariels-mbp.mynet>
2018-07-23 15:31:24 +0300

    WIP anim peds first pass

--- scripts/tweenModules.js
+++ scripts/tweenModules.js
@@ -39,7 +39,7 @@ export async function tweenCol(colToAnim, targetCol, options) {
 ////////////////////////////////////////////////////////////////////
 
 /* Animates a Vector3 to the target */
-export async function tweenVector3(vectorToAnimate, target, options) {
+export async function tweenThreeVec3(vectorToAnimate, target, options) {
   options = options || {};
   // get targets from options or set to defaults
   var to = target || THREE.Vector3(),
@@ -63,6 +63,35 @@ export async function tweenVector3(vectorToAnimate, target, options) {
   return tweenVector3;
 }
 
+////////////////////////////////////////////////////////////////////
+
+/* Animates a move to the target */
+export async function tweenMoveObj(Obj, target, options) {
+  options = options || {};
+  // get targets from options or set to defaults
+  var to = target || [0, 0, 0],
+    easing = options.easing || TWEEN.Easing.Quadratic.In,
+    duration = options.duration || 2000;
+  // create the tween
+  var tweenVector3 = new TWEEN.Tween(Obj)
+    .to({ x: to.x, y: to.y, z: to.z }, duration)
+    .easing(easing)
+    .onUpdate(function(d) {
+      if (options.update) {
+        options.update(d);
+      }
+    })
+    .onComplete(function() {
+      if (options.callback) options.callback();
+    });
+  // start the tween
+  tweenVector3.start();
+  // return the tween in case we want to manipulate it later on
+  return tweenVector3;
+}
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
 //Animation Loop method
 function animate(t) {
   requestAnimationFrame(animate);
