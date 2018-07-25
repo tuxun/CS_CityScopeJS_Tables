09f69bac9be6bd6be9eb3cd455f70b3c4e392560
RELNO <noyman@ariels-mbp.mynet>
2018-07-25 12:31:25 +0300

    peds cols

--- scripts/peds.js
+++ scripts/peds.js
@@ -1,26 +1,34 @@
 // https://github.com/mrdoob/three.js/blob/master/examples/webgl_buffergeometry_drawcalls.html
 import * as THREE from "THREE";
+import * as texPath from "../images/ball.png";
 
 export function makePeds(color, NeigbhorsArr, countRes) {
   var cellPeds;
   var geometry = new THREE.BufferGeometry();
   var positions = [];
   var colors = [];
+
+  var texture = new THREE.TextureLoader().load(texPath.default);
+
   var maxparticles = 100;
+
   //ratio of particles to num of neighbors
   var particles = (maxparticles * countRes) / NeigbhorsArr.length;
-  // //size of bounding box in THREE units
+
+  //size of bounding box in THREE units
   var n = 1;
   // particles spread in the cube
   var n2 = n / 2;
   for (var i = 0; i < particles; i++) {
+    // colors -WIP GET THIS IN RGB
+    colors.push(color[0]);
+    colors.push(color[1]);
+    colors.push(color[2]);
+
     // positions
     var x = Math.random() * n - n2;
-    // var y = Math.random() * n * 5;
     var z = Math.random() * n - n2;
     positions.push(x, 2, z);
-    // colors -WIP GET THIS IN RGB
-    colors.push(color[0], color[1], color[2]);
   }
 
   let posBuffer = new THREE.Float32BufferAttribute(positions, 3).setDynamic(
@@ -35,56 +43,63 @@ export function makePeds(color, NeigbhorsArr, countRes) {
 
   geometry.computeBoundingSphere();
   var material = new THREE.PointsMaterial({
-    size: 0.03,
-    vertexColors: THREE.VertexColors
+    size: 0.1,
+    transparent: true,
+    opacity: 0.5,
+    vertexColors: THREE.VertexColors,
+    blending: THREE.AdditiveBlending,
+    map: texture,
+    depthTest: false
   });
 
   cellPeds = new THREE.Points(geometry, material);
+  // console.log(cellPeds.geometry.attributes.color.array);
+
   //grab the peds pos array for animation
   var posArr = posBuffer.array;
 
   // animate
   function animate() {
     requestAnimationFrame(animate);
-    updatePositions(posArr);
+    updatePositions();
     cellPeds.geometry.attributes.position.needsUpdate = true;
     cellPeds.geometry.attributes.color.needsUpdate = true;
   }
 
-  //call anim at start
+  //call anim looper
   animate();
   //send back to scene
   return cellPeds;
 
   ////////////////////////////////////////
   function updatePositions() {
-    let index = 0;
+    let angle = 0;
     //run through location array [x,y,z,x,y,z..]
     //of all peds in this cell
 
     for (let i = 0; i < posArr.length; i = i + 3) {
       //set fix Y
-      posArr[i + 1] = 2;
+      posArr[i + 1] = 0;
 
       //set X NeigbhorsArr.length / 4
       if (posArr[i] >= 1 || posArr[i] <= -1) {
         posArr[i] = 0;
       } else if (posArr[i] < 1 || posArr[i] > -1) {
         // posArr[i] += Math.random() / 20;
-        posArr[i] += Math.cos(index) * 0.01;
+        posArr[i] += Math.cos(angle) * 0.01;
       }
 
       //set Z
       if (posArr[i + 2] >= 1 || posArr[i + 2] <= -1) {
         posArr[i + 2] = 0;
       } else if (posArr[i + 2] < 1 || posArr[i + 2] > -1) {
-        posArr[i + 2] += Math.sin(index) * 0.01;
+        posArr[i + 2] += Math.sin(angle) * 0.01;
       }
 
-      if (index < countRes * 360) {
-        index++;
+      if (angle < countRes * 360) {
+        angle++;
       } else {
-        index = 0;
+        angle = 0;
       }
     }
   }
