089df76280538f1ff3f57ead6674c6d80ff93b7e
Ariel Noyman <noyman@mit.edu>
2018-09-23 13:16:24 -0400

    sh for gh-pages

--- CSjs_MobCho/GHP.sh
+++ CSjs_MobCho/GHP.sh
@@ -1,17 +1,12 @@
 # #!/bin/bash
-# directory="./dist"
-
-# # bash check if dist directory already exists
-# if [ -d $directory ]; then
-# 	echo "Dist exists, removing"
+# clear dist folder 
 sudo rm -rf dist
-
 # build the dist for public url 
-sudo parcel build index.html --public-url https://cityscope.media.mit.edu/CS_CityScopeJS_Walk/
+sudo parcel build index.html --public-url https://cityscope.media.mit.edu/CS_CityScopeJS_Tables/
 # make sure to add dist 
 git add dist -f
 #commit the GH pages changes 
 git commit -m "gh-pages commit"
-#push to subtree remote 
+#push to subtree remote [Force] 
 # git subtree push --prefix dist origin gh-pages
 git push origin `git subtree split --prefix dist master`:gh-pages --force
