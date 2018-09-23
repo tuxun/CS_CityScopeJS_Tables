6dbb9a2170341059f74ad3f6f2106e27641ad6b0
Ariel Noyman <noyman@mit.edu>
2018-09-23 13:30:30 -0400

    ghp

--- GHP.sh
+++ GHP.sh
@@ -1,6 +1,6 @@
 # #!/bin/bash
 # clear dist folder 
-sudo rm -rf dist
+sudo rm -rf CSjs_MobCho/dist
 # build the dist for public url 
 sudo parcel build index.html --public-url https://cityscope.media.mit.edu/CS_CityScopeJS_Tables/
 # make sure to add dist 
@@ -10,3 +10,5 @@ git commit -m "gh-pages commit"
 #push to subtree remote [Force] 
 # git subtree push --prefix dist origin gh-pages
 git push origin `git subtree split --prefix dist master`:gh-pages --force
+
+
